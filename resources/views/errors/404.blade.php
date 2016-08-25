ca34191f8a5761a03e5b2c1d007ea00a5a0eb4d7
Gilberto Villani Brito <gilberto@insighti.com.br>
2016-08-25 20:06:07 -0300

    V 1.0.2 - Fixed some bugs. - Layout fixed for mobiles.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="center-block">
+            <div class="panel panel-default">
+                <div class="panel-heading">OOPS...</div>
+
+                <div class="panel-body">
+                    Esta página não existe.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
