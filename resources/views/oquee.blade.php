fd79e8c28e99e2aa02446b0e9bbbfc1eb12e84ba
Gilberto Villani Brito <gilberto@insighti.com.br>
2016-08-26 07:32:12 -0300

    V 1.0.2 - Fixed some bugs. - Layout fixed for mobiles.

--- resources/views/oquee.blade.php
+++ resources/views/oquee.blade.php
@@ -7,16 +7,20 @@
             <div class="panel panel-default">
                 <div class="panel-heading">O que é?</div>
 
-                <div class="panel-body">
+                <div class="panel-body text-justify">
                     <p>Despensa APP é um sistema simples para controlar a sua despensa de casa de qualquer lugar.<br>
                     O sistema pode ser acessado do seu computador ou celular desde que tenha acesso a internet.</p>
                     <br>
                     <h4>Como funciona?</h4>
-                    <p>Você irá cadastrar os produtos e a quantidade mínima que deseja ter na sua despensa.<br>
-                    Depois de realizado as compras no supermercado, você deve informar ao sistema o produto e a quantidade comprada e o mesmo irá atualizar a sua despensa.<br>
-                    A cada consumo de um produto da despensa, você deve informar no sistema o que foi consumido para gerar a lista de compras.<br>
-                    A lista de compras será gerada sempre com a quantidade necessária para atingir o mínimo de estoque na despensa.
-                    </p>
+                    <p>O Despensa APP tem o seguinte menu:</p>
+                    <img class="img-responsive" src="{{ url('/images/despensa1.png') }}">
+                    <li>1 - Cadastre as unidades dos produtos (nome).</li>
+                    <li>2 - Cadastre os produtos (nome e unidade).</li>
+                    <li>3 - Adicione os produtos que você quer ter na sua despensa (quantidade mínima).</li>
+                    <li>4 - Os produtos que você já tem na sua despensa ou que você comprou, devem ser atualizados para corrigir o estoque (quantidade comprada).</li>
+                    <li>5 - Os produtos que você consome devem ser atualizados aqui (quantidade consumida).</li>
+                    <p style="margin-top: 15px;">A lista de compras está no menu <strong>Comprar</strong> e será formada pelos produtos que você tem na despensa
+                        que estão com a quantidade abaixo do mínimo configurado na despensa.</p>
                     <br>
                     <h3>Use-o, é gratis.</h3>
                     <a class="btn btn-small btn-primary" href="{{ url('/register') }}">
